dc949574ffc236e8cef989a64f2ba501f367c129
Taniya1308 <taniyasharma1308@gmail.com>
2026-07-18 10:20:52 +0530

    fix backend deployment

--- server/index.js
+++ server/index.js
@@ -2,17 +2,22 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import generateRoutes from "./routes/generateRoutes.js";
+
 dotenv.config();
 
-const app = express(); //browser
+const app = express();
 
-app.use(cors()); //middlewares
-app.use(express.json()); //middlewares
+app.use(cors());
+app.use(express.json());
 
-app.use("/generate", generateRoutes); //register route
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully");
+});
+
+app.use("/generate", generateRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-}); //start server
+});
